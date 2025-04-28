8b07619204a58f19f42006eade663d7cc7e131e0
IgrisAbhisheksingh <ABHISHEK8801108@GMAIL.COM>
2025-04-28 21:11:28 +0530

    first

---
+++ 01_variable/01_sum.java
@@ -0,0 +1,8 @@
+class Main{
+    public static void main(String []args)
+    {
+        int a=10;
+        int b=12;
+        System.out.println("sum"+ (a+b));
+    }
+}
---
+++ 01_variable/02_input.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Input {
+    public static void main(String []args)
+    {
+        Scanner in =new Scanner(System.in);
+        System.out.println("enter a number ");
+        int number=in.nextInt();
+        System.out.println("your enter number "+ number);
+        in.close();
+
+    }
+    
+}
