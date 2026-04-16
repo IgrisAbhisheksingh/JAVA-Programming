d2e960419adf8f1ca1cf1e48627bc872a4ca6a14
Abhishek Singh <ABHISHEK8801108@GMAIL.COM>
2026-04-16 22:27:50 +0530

    lll

---
+++ GreedyAlgo/StandEndSpan.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class StandEndSpan {
+    public static void main(String [] args){
+
+        int []start={1,3,0,5,8,5};
+        int [] end={2,4,6,7,9,9};
+
+        int maxAct=0;
+        //end time basis sorted
+
+        ArrayList<Integer> ans=new ArrayList<>();
+
+        //1st activity
+        maxAct=1;
+        ans.add(0);
+        int lastEnd=end[0];
+        for(int i=1;i<end.length;i++){
+            if(start[i]>=lastEnd){
+                maxAct++;
+                lastEnd=end[i];
+            }
+        }
+        System.out.println("max activity =" + maxAct);
+    }
+}
